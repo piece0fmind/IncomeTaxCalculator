db92bb28bdaca6b26b3e8cdac7a9c2f17b4dff77
Bibek Neupane <bibek.neupane@amniltech.com>
2023-05-08 15:27:43 +0545

    Add test cases

---
+++ TaxCalculationConsoleApplication/TaxCalculation/TaxBracket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TaxCalculationConsoleApplication
+{
+	public class TaxBracket
+	{
+        public double LowerLimit { get; set; }
+        public double UpperLimit { get; set; }
+        public double TaxPercent { get; set; }
+    }
+}
+
---
+++ TaxCalculationTests/TaxCalculationTests.cs
@@ -0,0 +1,60 @@
+﻿namespace TaxCalculationTest;
+
+public class TaxCalculationTests
+{
+    private readonly Tax _tax;
+
+    public TaxCalculationTests()
+    {
+        _tax = new Tax();
+    }
+
+    [Fact]
+    public void CalculateTax_ShouldReturnCorrectTaxAmount()
+    {
+        double usualIncome = 400;
+        double additionalIncome = 1500;
+        double taxAmount = 240;
+
+        var result = _tax.CalculateTax(usualIncome, additionalIncome);
+
+        Assert.Equal(taxAmount, result);
+    }
+
+    [Fact]
+    public void CalculateTax_BothIncomesAreZero_ShouldReturnZero()
+    {
+        double usualIncome = 0;
+        double additionalIncome = 0;
+        double taxAmount = 0;
+
+        var result = _tax.CalculateTax(usualIncome, additionalIncome);
+
+        Assert.Equal(taxAmount, result);
+    }
+
+    [Fact]
+    public void CalculateTax_AdditionalIncomeIsZero_ShouldReturnZero()
+    {
+        double usualIncome = 1000;
+        double additionalIncome = 0;
+        double taxAmount = 0;
+
+        var result = _tax.CalculateTax(usualIncome, additionalIncome);
+
+        Assert.Equal(taxAmount, result);
+    }
+
+    [Fact]
+    public void CalculateTax_UsualIncomeIsZero_ShouldReturnZero()
+    {
+        double usualIncome = 0;
+        double additionalIncome = 0;
+        double taxAmount = 0;
+
+        var result = _tax.CalculateTax(usualIncome, additionalIncome);
+
+        Assert.Equal(taxAmount, result);
+    }
+
+}
