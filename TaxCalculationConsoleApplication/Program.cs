f3d2293fdfe26d6dfb178415488981ce66169a16
Bibek Neupane <bibek.neupane@amniltech.com>
2023-05-08 15:25:24 +0545

    Add tax calculation logic

---
+++ TaxCalculationConsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaxCalculationConsoleApplication;
+
+Console.WriteLine("Income Tax Calculation");
+
+Tax tax = new();
+Console.WriteLine($"Total tax = {tax.CalculateTax(10, 5000)}");
+
+Console.ReadKey();
---
+++ TaxCalculationConsoleApplication/TaxCalculation/Tax.cs
@@ -0,0 +1,49 @@
+﻿namespace TaxCalculationConsoleApplication
+{
+	public class Tax
+	{
+        private List<TaxBracket> TaxBrackets { get; set; }
+
+        public Tax()
+        {
+            TaxBrackets = new List<TaxBracket>
+            {
+                new TaxBracket { LowerLimit = 0, UpperLimit = 1000, TaxPercent = 0.1 },
+                new TaxBracket { LowerLimit = 1000, UpperLimit = 5000, TaxPercent = 0.2 },
+                new TaxBracket { LowerLimit = 5000, UpperLimit = double.PositiveInfinity, TaxPercent = 0.35 }
+            };
+        }
+
+
+        public double CalculateTax(double usualIncome, double additionalIncome)
+        {
+            double totalTax = 0;
+            var remainingAmountAfterTaxDeduction = additionalIncome;
+
+            foreach (var taxBracket in TaxBrackets)
+            {
+                if(usualIncome <= taxBracket.UpperLimit)
+                {
+                    double taxableIncome = Math.Min(remainingAmountAfterTaxDeduction, taxBracket.UpperLimit - usualIncome);
+                    double taxAmount = taxableIncome * taxBracket.TaxPercent;
+                    totalTax += taxAmount;
+                    remainingAmountAfterTaxDeduction -= taxableIncome;
+
+                }
+                if (remainingAmountAfterTaxDeduction == 0)
+                {
+                    break;
+                }
+
+                usualIncome = Math.Max(0, usualIncome - taxBracket.UpperLimit);
+
+
+            }
+
+            return totalTax;
+        }
+    }
+
+	
+}
+
